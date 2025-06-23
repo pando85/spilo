fe966688df304f428a1fd7cb7355817569e9d1cb
Alexander Gil <pando855@gmail.com>
2025-06-23 20:27:42 +0200

    feat: Add vchord 0.4.3

--- postgres-appliance/build_scripts/base.sh
+++ postgres-appliance/build_scripts/base.sh
@@ -149,6 +149,11 @@ for version in $DEB_PG_SUPPORTED_VERSIONS; do
         EXTRA_EXTENSIONS+=("plprofiler" "pg_mon-${PG_MON_COMMIT}")
     fi
 
+    if [ "$version" -ge 14 ]; then
+        curl -L https://github.com/tensorchord/VectorChord/releases/download/${VCHORD}/postgresql-${version}-vchord_${VCHORD}-1_amd64.deb > postgresql-${version}-vchord_${VCHORD}-1_amd64.deb
+        apt-get install ./postgresql-${version}-vchord_${VCHORD}-1_amd64.deb
+    fi
+
     for n in bg_mon-${BG_MON_COMMIT} \
             pg_auth_mon-${PG_AUTH_MON_COMMIT} \
             set_user \
